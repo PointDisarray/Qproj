33ef6e8b49a569ef49070ebb118eaa849078c15d
Piotr Rejczak <piotr.rejczak@britenet.com.pl>
2020-03-20 11:36:46 +0000

    #2 Quake Database Class

--- quake_db_class.py
+++ quake_db_class.py
@@ -7,8 +7,8 @@ class QuakeDatabase:
     def __init__(self, username, password, host, database):
         self.username = username
         self.password = password
-        self. host = host
-        self. database = database
+        self.host = host
+        self.database = database
 
     def __connect__(self):
         self.mydb = mysql.connector.connect(user=self.username, password=self.password,
@@ -17,6 +17,9 @@ class QuakeDatabase:
 
     def __disconnect__(self):
         self.mydb.close()
+
+    def __usedatabase__(self)
+	self.cursor.execute("USE "+self.database+";")
 
 
     def showDatabase(self):
@@ -29,10 +32,16 @@ class QuakeDatabase:
 
     def showTables(self):
         self.__connect__()
-        self.cursor.execute("USE "+self.database+";")
+        self.__usedatabase__()
         self.cursor.execute("SHOW TABLES;")
         result = self.cursor.fetchall()
         for db in result:
             print(db)
         self.__disconnect__()
 
+    def insertPlayer(self,player_id,player_name):
+        self.__connect__()
+        self.__usedatabase__()
+        self.cursor.execute("INSERT INTO players (id-player, name) \
+                            VALUES ('"+player_name+"')")
+        self.__disconnect__()
