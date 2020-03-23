c3ac9b0ac23cbfbb1f05fd45d03554698c40589c
Piotr Rejczak <piotr.rejczak@britenet.com.pl>
2020-03-23 13:23:50 +0000

    #5 Quake Database Class Test - Start Insert Section

--- quake_db_class.py
+++ quake_db_class.py
@@ -2,6 +2,7 @@
 
 import mysql.connector
 from mysql.connector import errorcode
+import os
 
 class QuakeDatabase:
 
@@ -11,41 +12,61 @@ class QuakeDatabase:
         self.host = host
         self.database = database
 
-    def __connect__(self):
+    def connect(self):
         try:
+           print("Connecting to " + self.database + " database, please wait ...")
+           os.system("sleep 2")
            self.mydb = mysql.connector.connect(user=self.username, password=self.password,
                                           host=self.host, database=self.database)
            self.cursor = self.mydb.cursor()
         except mysql.connector.Error as err:
-           print("Something went wrong: {}".format(err))
-           exit(1)
+           if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+              print("Something is wrong with your user name or password")
+              exit(1)
+           elif err.errno == errorcode.ER_BAD_DB_ERROR:
+              print("Database does not exist")
+              exit(1)
+           else:
+              print(err)
+              exit(1)
 
-    def __disconnect__(self):
+    def disconnect(self):
         self.mydb.close()
+        print("Disconnect from " + self.database + " ...")
+        os.system("sleep 2")
 
-    def __switch_database__(self):
+    def switch_database(self):
         self.cursor.execute("USE " + self.database + ";")
 
+    #def __dbInfoUpdate__(self, record, database_name):
+    #    print(record.upper()+" has been added to "+database_name.upper()+" database sucesfully")
+
     def showDatabase(self):
-        self.__connect__()
         self.cursor.execute("SHOW DATABASES;")
         result = self.cursor.fetchall()
         for db in result:
             print(db)
-        self.__disconnect__()
     
     def showTables(self):
-        self.__connect__()
-        self.__switch_database__()
         self.cursor.execute("SHOW TABLES;")
         result = self.cursor.fetchall()
         for db in result:
             print(db)
-        self.__disconnect__()
     
-    def insertPlayer(self,player_name):
-        self.__connect__()
-        self.__switch_database__()
+    def addPlayer(self, player_name):
         self.cursor.execute("INSERT INTO players (name) VALUES (\""+player_name+"\");")
         self.mydb.commit()
-        self.__disconnect__()
+        #__dbInfoUpdate__(player_name, self.database)
+        print(player_name.upper()+" has been added to "+self.database.upper()+" database sucesfully")
+        return self.cursor.lastrowid
+
+    def addMap(self, matche_date, matche_map, matche_type, matche_isTeamGame, matche_duration):
+        self.cursor.execute("INSERT INTO matches (datetime, map, type, isTeamGame, duration)\
+                             VALUES (\""+matche_date+"\",\""+matche_map+"\",\""+matche_type+"\","+matche_isTeamGame+","+matche_duration+");")
+        self.mydb.commit()
+        return self.cursor.lastrowid        
+
+    #def insertStats(self, stat_name, stat_value):
+    #    self.cursor.execute("INSERT INTO players (
+    
+    #def insertStats(self, stat_name, stat_value):
--- quake_main.py
+++ quake_main.py
@@ -3,9 +3,18 @@
 from quake_db_class import *
 
 dbQuake = QuakeDatabase('jeffrey', 'mypass', '127.0.0.1', 'stats')
-                                                                                                         
+
+dbQuake.connect()
+dbQuake.switch_database()
+                                                                                        
 dbQuake.showDatabase()
 print("================")
 dbQuake.showTables()
 print("================")
-dbQuake.insertPlayer("Rysio")
+player_id = dbQuake.addPlayer("Jasio")
+print("Player id = "+str(player_id))
+print("================")
+map_id = dbQuake.addMap("2019/09/03 15:46:21","cpm19","FFA",0,472)
+print("Map id = "+str(map_id))
+print("================")
+dbQuake.disconnect()
