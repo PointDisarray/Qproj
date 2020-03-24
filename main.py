38ceca7614da602968000d4d062f7262ec4b08b2
Yurii Lapenchuk <yurii.lapenchuk@britenet.com.pl>
2020-03-24 16:22:57 +0100

    created main.py

--- main.py
+++ main.py
@@ -11,15 +11,20 @@ dbQuake.switch_database()
 
 tmp_match_id = 0
 tmp_user_id = 0
-query_params = []
+data = []
 file_name = "data_from_15_53_50.xml"
 
 
-with open(file_name,"w+") as file:
-    for line in f.readlines():
+with open(file_name) as file:
+    for line in file.readlines():
         if "matches," in line:
-            query_params = line.strip().split(",")
-            print(query_params)
+            newline = line.replace('\'', '').replace(' ', '')
+            data = newline.strip().split(",")
+            print(data)
+            qwe = data[2]
+            print(qwe)
+            data[2] = data[2][ : 10] + " " + data[2][10 : ]
+            dbQuake.addMap(data[2], data[3], data[4], int(data[1]), int(data[6]))
         #if "players," in line:
 
         print("everything else")
